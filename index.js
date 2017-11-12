3dde4e910f803fd92b15dc78710070837fb7f31e
Max han <mwellh84@gmail.com>
2017-11-12 18:24:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution in this file.
+// Code your solution in this file
+function lowerCaseDrivers(array){
+  return array.map(function(e){
+    return e.toLowerCase()
+  })
+}
+
+function nameToAttributes(names){
+  names.map(function(name){
+    let fName = name.split(" ")[0]
+    let lName = name.split(" ")[1]
+    return {firstName: fName, lastName: lName}
+  })
+}
